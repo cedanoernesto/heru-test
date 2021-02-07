80a253d794d0117f2b1edfa3051a5a96df90e20f
Ernesto Cedano <cedanoernesto@gmail.com>
2021-02-08 00:33:17 +0100

    implemented eslint and pre-commit hook.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'react-native'
+  ],
+  extends: [
+    'eslint:recommended',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  rules: {
+    'no-console': 0,
+    'react-native/no-unused-styles': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2
+  }
 };
